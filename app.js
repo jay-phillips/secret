b55235ee5c9e682865175217fbf2681f03ce0296
Jerone Phillips <jeronephillips@outlook.com>
2020-06-26 14:45:22 -0400

    git commit

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const md5 = require("md5");
 const bcrypt = require("bcrypt");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 const saltRounds = 10;
+
+
 
 
 const app = express();
@@ -15,7 +22,19 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
+mongoose.set("useCreateIndex", true);
+
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -29,7 +48,27 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+
+const User =  new mongoose.model('User', userSchema);
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: procss.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
 
 
 app.get("/", function(req, res){
@@ -38,6 +77,9 @@ app.get("/", function(req, res){
 
 });
 
+app.get("/auth/google", 
+    passport.authenticate("google", { scope: ['profile']} )
+);
 
 app.get("/register", function(req, res){
 
@@ -47,9 +89,34 @@ app.get("/register", function(req, res){
 
 })
 
+app.get("/secrets", function(req, res){
+    if(req.isAuthenticated()){
+         res.render("secrets");
+    }else{
+        res.redirect("/login");
+    }
+ })
+ app.get("/logout", function(req, res){
+     req.logout();
+     res.redirect("/");
+ })
+
 app.post("/register", function(req, res){
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.redirect("/register")
+        } else {
+        passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+        })
+        }
+    })
+
+
+
+   /* bcrypt.hash(req.body.password, saltRounds, function(err, hash){
 
         const emailEntered = req.body.username;
        
@@ -73,7 +140,7 @@ app.post("/register", function(req, res){
         res.render("home");
     })
 
-    
+    */
 
   
 })
@@ -84,8 +151,30 @@ app.get("/login", function(req, res){
 })
 
 app.post("/login", function(req, res){
+
     
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+   const user = new User({
+       username: req.body.username,
+       password: req.body.password
+   });
+
+   req.login(user, function(err){
+        if(err){
+            console.log(err);
+        }else{
+          passport.authenticate("local")(req, res, function(){
+
+           res.redirect("/secrets");
+          })
+            
+        }
+
+   })
+
+
+
+    
+    /*bcrypt.hash(req.body.password, saltRounds, function(err, hash){
     const emailReq = req.body.username;
    
 
@@ -100,16 +189,11 @@ app.post("/login", function(req, res){
         }
 
     })
-        })
+})*/
 })
 
 
-app.get("/secrets", function(req, res){
 
-
-
-    res.render("secrets");
-})
 
 
 
